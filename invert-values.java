983cf84e078beb0fd36419c5e77b0e5f99e88d4b
Rodrigo L Magdaleno <107630527+rodlmag@users.noreply.github.com>
2022-10-25 12:17:00 -0500

    Option 1 added

--- invert-values.java
+++ invert-values.java
@@ -3,6 +3,23 @@ Given a set of numbers, return the additive inverse of each.
 Each positive becomes negatives, and the negatives become positives.
 */
 
+/* OPTION 1 */
+
+public class Kata {
+  public static int[] invert(int[] array) {
+    int [] inverted = new int [array.length];
+    int i = 0;
+    
+    for(int n: array){
+        inverted[i] = n * -1;
+        i++;
+      }
+    return inverted;
+  }
+}
+
+
+/* OPTION 2 */
 import java.lang.Math;
 
 public class Kata {
